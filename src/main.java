b742c7f6cae8af25f240df3159122185d8ade587
HUSEYIN6134 <huseyin.murat@st.uskudar.edu.tr>
2022-12-04 15:20:07 +0300

    Inheritance
    Engin Demiroğ Java Kursu

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+		
+		EmployeeManager employeemanager = new EmployeeManager();
+		CustomerManager customermanager = new CustomerManager();
+		
+
+	}
+
+}
